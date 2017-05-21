cb46fb4121ab47d63bde1d5440793cf947691ae4
ondraczsk <ondrakraftak@gmail.com>
2017-05-21 08:32:25 +0200

    Update Main.php

--- src/xFlare/xRulesx/Main.php
+++ src/xFlare/xRulesx/Main.php
@@ -18,30 +18,77 @@ class Main extends PluginBase implements Listener{
 	public function onCommand(CommandSender $sender, Command $command, $label, array $args) {
         $cmd = strtolower($command->getName());
         switch ($cmd){
-            case "rules":
+            case "pravidla":
                 if (!($sender instanceof Player)){
-                    $sender->sendMessage(TEXTFORMAT::GOLD . "--------[xRulesx]--------");
-                    $sender->sendMessage(TEXTFORMAT::GREEN . "- " . $this->getConfig()->get("rule1"));
-                    $sender->sendMessage(TEXTFORMAT::GREEN . "- " . $this->getConfig()->get("rule2"));
-                    $sender->sendMessage(TEXTFORMAT::GREEN . "- " . $this->getConfig()->get("rule3"));
-                    $sender->sendMessage(TEXTFORMAT::GREEN . "- " . $this->getConfig()->get("rule4"));
-                    $sender->sendMessage(TEXTFORMAT::GREEN . "- " . $this->getConfig()->get("rule5"));
-                    $sender->sendMessage(TEXTFORMAT::GREEN . "- " . $this->getConfig()->get("rule6"));
-                    $sender->sendMessage(TEXTFORMAT::GREEN . "- " . $this->getConfig()->get("rule7"));
-                    $sender->sendMessage(TEXTFORMAT::GREEN . "- " . $this->getConfig()->get("rule8"));
+                    $sender->sendMessage(TEXTFORMAT::GOLD . "§f§l--------* §e§oPravidla §r§f§l*--------")
+                    $sender->sendMessage(TEXTFORMAT::RED . "- " . $this->getConfig()->get("rule1"));
+                    $sender->sendMessage(TEXTFORMAT::RED . "- " . $this->getConfig()->get("rule2"));
+                    $sender->sendMessage(TEXTFORMAT::RED . "- " . $this->getConfig()->get("rule3"));
+                    $sender->sendMessage(TEXTFORMAT::RED . "- " . $this->getConfig()->get("rule4"));
+                    $sender->sendMessage(TEXTFORMAT::RED . "- " . $this->getConfig()->get("rule5"));
+    
+		if($this->getConfigValue("ruleother1") === true)
+        {
+           $sender->sendMessage(TEXTFORMAT::RED . "- " . $this->getConfig()->get("ruleothermessage1"));
+        }
+        else
+        {
+          $this->getLogger()->info(TEXTFORMAT::AQUA.  " Ignoring other rule 1");  
+        }
+			
+				if($this->getConfigValue("ruleother2") === true)
+        {
+           $sender->sendMessage(TEXTFORMAT::RED . "- " . $this->getConfig()->get("ruleothermessage2"));
+        }
+        else
+        {
+          $this->getLogger()->info(TEXTFORMAT::AQUA.  " Ignoring other rule 2");  
+        }
+			
+				if($this->getConfigValue("ruleother3") === true)
+        {
+           $sender->sendMessage(TEXTFORMAT::RED . "- " . $this->getConfig()->get("ruleothermessage3"));
+        }
+        else
+        {
+          $this->getLogger()->info(TEXTFORMAT::AQUA.  " Ignoring other rule 3");  
+        }
                     return true;
                 }
                 $player = $this->getServer()->getPlayer($sender->getName());
-                if ($player->hasPermission("xflare.rules")){
-                    $sender->sendMessage("--------[xRulesx]--------");
+                if ($player->hasPermission("xflare.pravidla")){
+                    $sender->sendMessage("--------*pravidla*--------");
                     $sender->sendMessage("- " . $this->getConfig()->get("rule1"));
                     $sender->sendMessage("- " . $this->getConfig()->get("rule2"));
                     $sender->sendMessage("- " . $this->getConfig()->get("rule3"));
                     $sender->sendMessage("- " . $this->getConfig()->get("rule4"));
                     $sender->sendMessage("- " . $this->getConfig()->get("rule5"));
-                    $sender->sendMessage("- " . $this->getConfig()->get("rule6"));
-                    $sender->sendMessage("- " . $this->getConfig()->get("rule7"));
-                    $sender->sendMessage("- " . $this->getConfig()->get("rule8"));
+                    if($this->getConfigValue("ruleother1") === true)
+        {
+           $sender->sendMessage(TEXTFORMAT::RED . "- " . $this->getConfig()->get("ruleothermessage1"));
+        }
+        else
+        {
+          $this->getLogger()->info(TEXTFORMAT::AQUA.  " Ignoring other rule 1");  
+        }
+			
+				if($this->getConfigValue("ruleother2") === true)
+        {
+           $sender->sendMessage(TEXTFORMAT::RED . "- " . $this->getConfig()->get("ruleothermessage2"));
+        }
+        else
+        {
+          $this->getLogger()->info(TEXTFORMAT::AQUA.  " Ignoring other rule 2");  
+        }
+			
+				if($this->getConfigValue("ruleother3") === true)
+        {
+           $sender->sendMessage(TEXTFORMAT::RED . "- " . $this->getConfig()->get("ruleothermessage3"));
+        }
+        else
+        {
+          $this->getLogger()->info(TEXTFORMAT::AQUA.  " Ignoring other rule 3");  
+        }
                     return true;
                 }
                 break;
